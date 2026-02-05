5bd4e78a3a2d0935b804bd11fff71fd53ccdcd8f
animeshsahu <anisahu99@gmail.com>
2026-02-06 00:01:07 +0530

    project initialization

---
+++ src/test/java/com/clinic/doc_appointment/DocAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.doc_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
